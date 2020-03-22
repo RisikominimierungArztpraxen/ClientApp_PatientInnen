592dd6572f0a7651bae5ff1e9778a6405f612424
Philipp Endrulat <philipp.endrulat@thinkproject.com>
2020-03-22 10:43:40 +0100

    init webserver

---
+++ src/main/java/de/risikominimierungarztpraxen/clientapp/ClientappApplication.java
@@ -0,0 +1,13 @@
+package de.risikominimierungarztpraxen.clientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientappApplication.class, args);
+	}
+
+}
